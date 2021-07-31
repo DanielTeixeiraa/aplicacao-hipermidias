58d8c8c067da15dba233add47a9d2a0d90ca503a
Daniel <Daniel.teixeirari@gmail.com>
2021-07-30 22:11:34 -0300

    Eventos (Hide/Show)

---
+++ curso/capitulo3/exercicios/javascript/script7.js
@@ -0,0 +1,26 @@
+function hide() {
+  var x = document.getElementById("conteudo_1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hide2() {
+  var y = document.getElementById("conteudo_2");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
+
+function hide3() {
+  var z = document.getElementById("conteudo_3");
+  if (z.style.display === "none") {
+    z.style.display = "block";
+  } else {
+    z.style.display = "none";
+  }
+}
